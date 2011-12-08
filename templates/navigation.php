1f44fe320ef32893e31b41bd1da54e977b873a86
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-12-08 22:38:28 +0800

    first commit templates

---
+++ templates/navigation.php
@@ -0,0 +1,53 @@
+<style type="text/css">
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+</style>
+<ul>
+     <li> 
+      <a href="admin_out.php">
+       <strong>
+         <font color="#FFFBF0"face="Arial Rounded MT Bold" size="3">
+          Sign Out
+         </font>
+       </strong>
+			</a>
+      </li>
+    <li> 
+     <a href="register.php">
+      <strong>
+        <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+           Sign Up
+        </font>
+      </strong>
+     </a>
+    </li>
+     <li> 
+      <a href="admin_portal.php">
+        <strong>
+          <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+            View User
+          </font>
+        </strong>
+      </a>
+     </li> 
+     <li>     
+      <a href="admin_logs.php">
+       <strong>
+         <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+           View Logs
+         </font>
+       </strong>
+      </a>
+     </li> 
+     
+</ul>
---
+++ templates/template.php
@@ -0,0 +1,9 @@
+<div class="header">
+<br>
+  	<div class = "logo_box">
+  		<img src="templates/images/people_hub.png" width="164" height="128" align="left" style="margin-left:20px;" />
+	</div>
+</div>
+
+
+
